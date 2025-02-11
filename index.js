db8576199923f69639bbd4d5a1f0fb3f7610b647
simpwae <naqashamjad777@gmail.com>
2025-02-11 23:24:07 +0500

    add basic express code with dotenv

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
